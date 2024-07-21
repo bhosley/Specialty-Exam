a97c9b11c92597ba6254549ffd481a10395bfcaa
bhosley <brandonscotthosley@gmail.com>
2024-07-21 12:47:04 -0400

    Update to setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,25 @@
-pip install ray[rllib,tune,air] wandb
-pip install swig box2d-py gymnasium[box2d]
+#!/bin/bash
 
-wandb login
+# update environment's package manager
+apt update && apt upgrade -y
+python3 -m pip install --upgrade pip
+
+# install python requirements
+python3 -m pip install -r requirements.txt
+
+# install WandB if it will be used
+echo -e "\n\n"
+read -p "Do you wish to use WandB (y/n)?" yesno
+case $yesno in
+    [Yy]* ) 
+        pip install wandb
+        wandb login
+    ;;
+    * ) 
+    echo "If you change your mind, execute:"
+    echo "pip install wandb"
+    echo "wandb login"
+    ;;
+esac
+
+echo -e "\nSetup complete"
